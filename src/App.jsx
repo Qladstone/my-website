39856db666f9a317ba146adaa6e1b24fff44dbef
Qladstone <looquanxiang@gmail.com>
2025-07-15 16:32:23 +0800

    Basic website setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import qladstoneLogo from './assets/Qladstone_logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="http://qladstone.com" target="_blank">
+          <img src={qladstoneLogo} className="logo" alt="Qladstone" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h3>Welcome to www.qladstone.com.</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+           
+        </p>
+        <p>
+          Qladstone is a backend software engineer based in Singapore.<br />
+          LinkedIn: <a href='http://linkedin.com/in/looquanxiang'>linkedin.com/in/looquanxiang</a><br />
+          GitHub: <a href='http://github.com/Qladstone'>github.com/Qladstone</a><br />
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This website is built using Vite and React, and deployed on Vultr.
       </p>
     </>
   )
